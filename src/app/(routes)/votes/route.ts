fe406c270eca7d3ede33778dbca5ab0f87743edf
Jason Schrader <whoabuddy@users.noreply.github.com>
2025-03-20 15:17:15 -0700

    chore: remove unused var, use correct return type

--- src/app/(routes)/votes/route.ts
+++ src/app/(routes)/votes/route.ts
@@ -1,4 +1,4 @@
-import { Cl, cvToValue, fetchCallReadOnlyFunction } from "@stacks/transactions";
+import { Cl, fetchCallReadOnlyFunction } from "@stacks/transactions";
 import { STACKS_TESTNET, STACKS_MAINNET } from "@stacks/network";
 import { NextResponse } from "next/server";
 
@@ -55,10 +55,10 @@ export async function GET(request: Request) {
             network,
         });
 
-        return {
+        return NextResponse.json({
             success: true,
             message: String(result),
-        };
+        });
 
         /*
 
