9724178009bfafb2c0eeec4bac1f5c4b57ba4185
biwasbhandari <biwas2059@gmail.com>
2025-03-22 10:09:55 +0545

    Revert "fix: fetch options"
    This reverts commit 930b6222731869d6e7aba3cf1bcfccd3614aa98d.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,14 +1,10 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
-import { getFetchOptions, setFetchOptions } from "@stacks/common";
+import { getFetchOptions } from "@stacks/network-v6"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-type StacksRequestInit = RequestInit & {
-  referrerPolicy?: string;
-};
-const fetchOptions: StacksRequestInit = getFetchOptions();
-delete fetchOptions.referrerPolicy;
-setFetchOptions(fetchOptions);
+const opts = getFetchOptions()
+delete opts.referrerPolicy
