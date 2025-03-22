c193f42f534afddb92c2cc81a024727aefafcc71
biwasbhandari <biwas2059@gmail.com>
2025-03-22 10:04:51 +0545

    Revert "fix: common->network"
    This reverts commit fbbf52e121e4cb5bb2ea7e3770eb37fb257be2ac.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
-import { getFetchOptions, setFetchOptions } from "@stacks/network";
+import { getFetchOptions, setFetchOptions } from "@stacks/common";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
