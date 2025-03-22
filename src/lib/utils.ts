fbbf52e121e4cb5bb2ea7e3770eb37fb257be2ac
biwasbhandari <biwas2059@gmail.com>
2025-03-22 09:20:00 +0545

    fix: common->network

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
-import { getFetchOptions, setFetchOptions } from "@stacks/common";
+import { getFetchOptions, setFetchOptions } from "@stacks/network";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
