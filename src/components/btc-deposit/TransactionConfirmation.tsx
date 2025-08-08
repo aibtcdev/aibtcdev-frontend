a696203f2bba0145729857c579db97f3d73aef76
biwasbhandari <biwas2059@gmail.com>
2025-08-08 11:03:18 +0545

    update

--- src/components/btc-deposit/TransactionConfirmation.tsx
+++ src/components/btc-deposit/TransactionConfirmation.tsx
@@ -315,6 +315,7 @@ export default function TransactionConfirmation({
     confirmationData.depositAmount,
     getBuyQuote,
     confirmationData.userInputAmount,
+    swapType,
   ]);
 
   useEffect(() => {
