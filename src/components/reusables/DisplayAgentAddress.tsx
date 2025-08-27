e88181ca4802a4c687e4120ed57940f411845d9a
biwasbhandari <biwas2059@gmail.com>
2025-08-27 18:13:06 +0545

    display user address instead of agent

--- src/components/reusables/DisplayAgentAddress.tsx
+++ src/components/reusables/DisplayAgentAddress.tsx
@@ -1,19 +1,13 @@
 "use client";
 
 import { useAuth } from "@/hooks/useAuth";
-import { useQuery } from "@tanstack/react-query";
-import { fetchAgents } from "@/services/agent.service";
 import { User } from "lucide-react";
+import { getStacksAddress } from "@/lib/address";
 
 const DisplayAgentAddress = () => {
   const { userId, isLoading: isSessionLoading } = useAuth();
 
-  const { data: agents = [] } = useQuery({
-    queryKey: ["agents"],
-    queryFn: fetchAgents,
-  });
-  const userAgent = agents[0] || null;
-  const userAgentAddress = userAgent?.account_contract || null;
+  const stacksAddress = userId ? getStacksAddress() : null;
 
   if (isSessionLoading || !userId) {
     return null;
@@ -23,9 +17,7 @@ const DisplayAgentAddress = () => {
     <div className="flex items-center gap-2 justify-center">
       <div className="flex items-center gap-1.5">
         <span className="font-inter font-bold tracking-tight text-sm break-all">
-          {userAgentAddress
-            ? `${userAgentAddress.slice(0, 5)}...${userAgentAddress.slice(-5)}`
-            : "No agent address"}
+          {`${stacksAddress?.slice(0, 5)}...${stacksAddress?.slice(-5)}`}
         </span>
         <div className="w-6 h-6 rounded-full border border-white flex items-center justify-center">
           <User className="w-3.5 h-3.5 " />
