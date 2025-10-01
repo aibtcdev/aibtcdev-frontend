b4c5fe1c2dcac212a77d6914dc16b99d0fdf4c93
biwasbhandari <biwas2059@gmail.com>
2025-10-01 15:59:21 +0545

    feat(notifications): Implement deposit notification banner
    Introduces a prominent banner to alert users of new DAO token deposits to their agent.
    
    This change replaces the previous bell icon notification system. It also standardizes balance formatting in deposit and withdraw modals by utilizing the new BalanceDisplay component.

--- src/components/notifications/index.ts
+++ src/components/notifications/index.ts
@@ -1,4 +1,5 @@
 export { NotificationProvider, useNotifications } from "./NotificationProvider";
 export { NotificationBell } from "./NotificationBell";
 export { NotificationDropdown } from "./NotificationDropdown";
+export { DepositNotificationBanner } from "./DepositNotificationBanner";
 export type { Notification, NotificationContextType } from "./types";
