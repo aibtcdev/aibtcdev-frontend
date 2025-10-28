169f593e55fa4c1fd34c5edc6bb8af2d9a09f692
Jason Schrader <whoabuddy@users.noreply.github.com>
2025-10-28 06:02:00 -0700

    fix: remove single dao mode (temp)

--- src/config/features.ts
+++ src/config/features.ts
@@ -1,2 +1,4 @@
-export const enableSingleDaoMode = true;
+// when enabled will show only a single DAO throughout the app
+export const enableSingleDaoMode = false;
+// only required when single DAO mode is enabled
 export const singleDaoName = "AIBTC";
