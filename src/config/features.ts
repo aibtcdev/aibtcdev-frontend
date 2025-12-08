e667363b4f1677518403277f38b811fd4d9ffa6f
Jason Schrader <whoabuddy@users.noreply.github.com>
2025-12-08 12:48:46 -0700

    fix: update reward amount too

--- src/config/features.ts
+++ src/config/features.ts
@@ -2,7 +2,5 @@
 export const enableSingleDaoMode = true;
 // only required when single DAO mode is enabled
 export const singleDaoName = "AIBTC-R1";
-
-// Reward configuration
-// Reward amount in USD for each passed proposal
-export const rewardPerPassedProposal = 50;
+// reward amount in USD for each passed proposal
+export const rewardPerPassedProposal = 1;
